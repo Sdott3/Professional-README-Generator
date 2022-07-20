dbfdbc6937d09d18e0b7a49afbb99d9cf37524e1
sdott3 <sdotthomas347@gmail.com>
2022-07-20 10:37:10 -0400

    recopying starter code

---
+++ Develop/index.js
@@ -0,0 +1,32 @@
+// TODO: Include packages needed for this application
+const fs = require('inquire');
+const inquirer = require('inquirer');
+const generateMarkdown = require('./utils/generateMarkdown.js');
+
+// TODO: Create an array of questions for user input
+const questions = [
+    // Title of project
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is the title of the project? (Required)',
+        validate: titleInput => {
+            if (titleInput) {
+                return true;
+            } else {
+                console.log('Please enter a title of your project!');
+                return false;
+            }
+        }
+    }
+];
+
+
+// TODO: Create a function to write README file
+function writeToFile(fileName, data) {}
+
+// TODO: Create a function to initialize app
+function init() {}
+
+// Function call to initialize app
+init();
